979575aa0e9fa1c48fd614692fd44bcc54d81688
Mukherjee, Subhadeep <Subhadeep.Mukherjee@tabcorp.com.au>
2024-05-28 13:02:12 +0530

    adding prefix as WorkShop and also have other changes as suggested

--- update-backend-stack-version/updatebackendstack.sh
+++ update-backend-stack-version/updatebackendstack.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-aws cloudformation update-stack --stack-name BackendStack --template-body file://backend.yaml --parameters \
+# Define stack names with WorkShop prefix
+STACK_NAME="WorkShop-BackendStack"
+SUBNETS_STACK_NAME="WorkShop-SubnetsStack"
+SECURITY_GROUPS_STACK_NAME="WorkShop-SecurityGroupsStack"
+LOAD_BALANCER_STACK_NAME="WorkShop-LoadBalancerStack"
+
+# Update the stack with the specified parameters
+aws cloudformation update-stack --stack-name $STACK_NAME --template-body file://backend.yaml --parameters \
 ParameterKey=KeyName,ParameterValue=key \
-ParameterKey=PrivateSubnet1Id,ParameterValue=$(aws cloudformation describe-stacks --stack-name SubnetsStack --query "Stacks[0].Outputs[?OutputKey=='PrivateSubnet1Id'].OutputValue" --output text) \
-ParameterKey=PrivateSubnet2Id,ParameterValue=$(aws cloudformation describe-stacks --stack-name SubnetsStack --query "Stacks[0].Outputs[?OutputKey=='PrivateSubnet2Id'].OutputValue" --output text) \
-ParameterKey=BackendAutoScalingSecurityGroupId,ParameterValue=$(aws cloudformation describe-stacks --stack-name SecurityGroupsStack --query "Stacks[0].Outputs[?OutputKey=='BackendAutoScalingSecurityGroupId'].OutputValue" --output text) \
-ParameterKey=BackendTargetGroupArn,ParameterValue=$(aws cloudformation describe-stacks --stack-name LoadBalancerStack --query "Stacks[0].Outputs[?OutputKey=='BackendTargetGroupArn'].OutputValue" --output text) \
+ParameterKey=PrivateSubnet1Id,ParameterValue=$(aws cloudformation describe-stacks --stack-name $SUBNETS_STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='PrivateSubnet1Id'].OutputValue" --output text) \
+ParameterKey=PrivateSubnet2Id,ParameterValue=$(aws cloudformation describe-stacks --stack-name $SUBNETS_STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='PrivateSubnet2Id'].OutputValue" --output text) \
+ParameterKey=BackendAutoScalingSecurityGroupId,ParameterValue=$(aws cloudformation describe-stacks --stack-name $SECURITY_GROUPS_STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='BackendAutoScalingSecurityGroupId'].OutputValue" --output text) \
+ParameterKey=BackendTargetGroupArn,ParameterValue=$(aws cloudformation describe-stacks --stack-name $LOAD_BALANCER_STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='BackendTargetGroupArn'].OutputValue" --output text) \
 ParameterKey=BackendDeployedVersion,ParameterValue=2.0.0 \
 ParameterKey=IndexHTMLContent,ParameterValue="Hello From Backend!! You have Deployed Version 2.0.0!!" \
 --capabilities CAPABILITY_IAM
