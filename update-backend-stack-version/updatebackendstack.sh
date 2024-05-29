b838733d1d72485190a20be5b1b2afe03d7aab04
deep-devsecops <167949771+deep-devsecops@users.noreply.github.com>
2024-05-29 17:32:22 +0530

    Update updatebackendstack.sh

--- update-backend-stack-version/updatebackendstack.sh
+++ update-backend-stack-version/updatebackendstack.sh
@@ -7,13 +7,12 @@ SECURITY_GROUPS_STACK_NAME="WorkShop-SecurityGroupsStack"
 LOAD_BALANCER_STACK_NAME="WorkShop-LoadBalancerStack"
 
 # Update the stack with the specified parameters
-aws cloudformation update-stack --stack-name $STACK_NAME --template-body file://backend.yaml --parameters \
+aws cloudformation update-stack --stack-name $STACK_NAME --profile workshop-admin --template-body file://backend.yaml --parameters \
 ParameterKey=KeyName,ParameterValue=workshop \
-ParameterKey=PrivateSubnet1Id,ParameterValue=$(aws cloudformation describe-stacks --stack-name $SUBNETS_STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='PrivateSubnet1Id'].OutputValue" --output text) \
-ParameterKey=PrivateSubnet2Id,ParameterValue=$(aws cloudformation describe-stacks --stack-name $SUBNETS_STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='PrivateSubnet2Id'].OutputValue" --output text) \
-ParameterKey=BackendAutoScalingSecurityGroupId,ParameterValue=$(aws cloudformation describe-stacks --stack-name $SECURITY_GROUPS_STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='BackendAutoScalingSecurityGroupId'].OutputValue" --output text) \
-ParameterKey=BackendTargetGroupArn,ParameterValue=$(aws cloudformation describe-stacks --stack-name $LOAD_BALANCER_STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='BackendTargetGroupArn'].OutputValue" --output text) \
+ParameterKey=PrivateSubnet1Id,ParameterValue=$(aws cloudformation describe-stacks --stack-name $SUBNETS_STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='PrivateSubnet1Id'].OutputValue" --output text --profile workshop-admin) \
+ParameterKey=PrivateSubnet2Id,ParameterValue=$(aws cloudformation describe-stacks --stack-name $SUBNETS_STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='PrivateSubnet2Id'].OutputValue" --output text --profile workshop-admin) \
+ParameterKey=BackendAutoScalingSecurityGroupId,ParameterValue=$(aws cloudformation describe-stacks --stack-name $SECURITY_GROUPS_STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='BackendAutoScalingSecurityGroupId'].OutputValue" --output text --profile workshop-admin) \
+ParameterKey=BackendTargetGroupArn,ParameterValue=$(aws cloudformation describe-stacks --stack-name $LOAD_BALANCER_STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='BackendTargetGroupArn'].OutputValue" --output text --profile workshop-admin) \
 ParameterKey=BackendDeployedVersion,ParameterValue=2.0.0 \
 ParameterKey=IndexHTMLContent,ParameterValue="Hello From Backend!! You have Deployed Version 2.0.0!!" \
 --capabilities CAPABILITY_IAM
-
