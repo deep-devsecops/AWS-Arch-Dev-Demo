6f4ae12ba6c309416cd2c3c590672bbc71f702aa
deep-devsecops <167949771+deep-devsecops@users.noreply.github.com>
2024-05-29 17:34:18 +0530

    Update createFrontendStack.sh

--- WorkShop-frontend-stack/createFrontendStack.sh
+++ WorkShop-frontend-stack/createFrontendStack.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-aws cloudformation create-stack --stack-name WorkShop-FrontendStack --template-body file://frontend.yaml --parameters \
+aws cloudformation create-stack --stack-name WorkShop-FrontendStack --profile workshop-admin --template-body file://frontend.yaml --parameters \
 ParameterKey=KeyName,ParameterValue=workshop \
-ParameterKey=PrivateSubnet1Id,ParameterValue=$(aws cloudformation describe-stacks --stack-name WorkShop-SubnetsStack --query "Stacks[0].Outputs[?OutputKey=='PrivateSubnet1Id'].OutputValue" --output text) \
-ParameterKey=PrivateSubnet2Id,ParameterValue=$(aws cloudformation describe-stacks --stack-name WorkShop-SubnetsStack --query "Stacks[0].Outputs[?OutputKey=='PrivateSubnet2Id'].OutputValue" --output text) \
-ParameterKey=FrontendAutoScalingSecurityGroupId,ParameterValue=$(aws cloudformation describe-stacks --stack-name WorkShop-SecurityGroupsStack --query "Stacks[0].Outputs[?OutputKey=='FrontendAutoScalingSecurityGroupId'].OutputValue" --output text) \
-ParameterKey=FrontendTargetGroupArn,ParameterValue=$(aws cloudformation describe-stacks --stack-name WorkShop-LoadBalancerStack --query "Stacks[0].Outputs[?OutputKey=='FrontendTargetGroupArn'].OutputValue" --output text) \
-ParameterKey=BackendLoadBalancerDNS,ParameterValue=$(aws cloudformation describe-stacks --stack-name WorkShop-LoadBalancerStack --query "Stacks[0].Outputs[?OutputKey=='BackendLoadBalancerDNS'].OutputValue" --output text) \
+ParameterKey=PrivateSubnet1Id,ParameterValue=$(aws cloudformation describe-stacks --stack-name WorkShop-SubnetsStack --query "Stacks[0].Outputs[?OutputKey=='PrivateSubnet1Id'].OutputValue" --output text --profile workshop-admin) \
+ParameterKey=PrivateSubnet2Id,ParameterValue=$(aws cloudformation describe-stacks --stack-name WorkShop-SubnetsStack --query "Stacks[0].Outputs[?OutputKey=='PrivateSubnet2Id'].OutputValue" --output text --profile workshop-admin) \
+ParameterKey=FrontendAutoScalingSecurityGroupId,ParameterValue=$(aws cloudformation describe-stacks --stack-name WorkShop-SecurityGroupsStack --query "Stacks[0].Outputs[?OutputKey=='FrontendAutoScalingSecurityGroupId'].OutputValue" --output text --profile workshop-admin) \
+ParameterKey=FrontendTargetGroupArn,ParameterValue=$(aws cloudformation describe-stacks --stack-name WorkShop-LoadBalancerStack --query "Stacks[0].Outputs[?OutputKey=='FrontendTargetGroupArn'].OutputValue" --output text --profile workshop-admin) \
+ParameterKey=BackendLoadBalancerDNS,ParameterValue=$(aws cloudformation describe-stacks --stack-name WorkShop-LoadBalancerStack --query "Stacks[0].Outputs[?OutputKey=='BackendLoadBalancerDNS'].OutputValue" --output text --profile workshop-admin) \
 --capabilities CAPABILITY_NAMED_IAM
