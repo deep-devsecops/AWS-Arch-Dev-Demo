4d585b830fb5aee382bc6ee01b8bbb20ab1ae923
deep-devsecops <167949771+deep-devsecops@users.noreply.github.com>
2024-05-29 17:36:17 +0530

    Update createSgStack.sh

--- WorkShop-security-groups/createSgStack.sh
+++ WorkShop-security-groups/createSgStack.sh
@@ -1,3 +1,3 @@
-aws cloudformation create-stack --stack-name WorkShop-SecurityGroupsStack --template-body file://security-groups.yaml --parameters \
-ParameterKey=VpcId,ParameterValue=$(aws cloudformation describe-stacks --stack-name WorkShop-VPCStack --query "Stacks[0].Outputs[?OutputKey=='VpcId'].OutputValue" --output text)
-
+#!/bin/bash
+aws cloudformation create-stack --stack-name WorkShop-SecurityGroupsStack --profile workshop-admin --template-body file://security-groups.yaml --parameters \
+ParameterKey=VpcId,ParameterValue=$(aws cloudformation describe-stacks --stack-name WorkShop-VPCStack --query "Stacks[0].Outputs[?OutputKey=='VpcId'].OutputValue" --output text --profile workshop-admin)
