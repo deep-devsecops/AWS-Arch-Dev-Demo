3c6a4956d77f14c0eeea7bfb8e5fcd6d021a7d02
deep-devsecops <167949771+deep-devsecops@users.noreply.github.com>
2024-05-29 17:36:40 +0530

    Update createSubnetsStack.sh

--- WorkShop-subnet-stack/createSubnetsStack.sh
+++ WorkShop-subnet-stack/createSubnetsStack.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-aws cloudformation create-stack --stack-name WorkShop-SubnetsStack --template-body file://subnet.yaml --parameters \
-ParameterKey=VpcId,ParameterValue=$(aws cloudformation describe-stacks --stack-name WorkShop-VPCStack --query "Stacks[0].Outputs[?OutputKey=='VpcId'].OutputValue" --output text)
-
+aws cloudformation create-stack --stack-name WorkShop-SubnetsStack --template-body file://subnet.yaml --parameters --profile workshop-admin \
+ParameterKey=VpcId,ParameterValue=$(aws cloudformation describe-stacks --stack-name WorkShop-VPCStack --query "Stacks[0].Outputs[?OutputKey=='VpcId'].OutputValue" --output text --profile workshop-admin)
